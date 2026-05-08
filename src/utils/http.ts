f0b49470cef1a0050d30bd6f92b9a29196436c47
Juan David Morantes Vergara <juan2191931@correo.uis.edu.co>
2026-05-08 09:38:46 -0500

    path base cd

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,4 @@
-const BASE_PATH = '/api/sigha-ia'
+const BASE_PATH = '/api/sigha-cd'
 
 export const apiFetch = (path: string, options?: RequestInit): Promise<Response> =>
   fetch(`${BASE_PATH}${path}`, options)
