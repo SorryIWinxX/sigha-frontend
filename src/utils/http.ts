3a69c942eaf9fc5b4b6b2237262e90ed91346342
Juan David Morantes Vergara <juan2191931@correo.uis.edu.co>
2026-05-08 09:32:40 -0500

    sigha-IA yml

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,4 @@
-const BASE_PATH = '/api/sigha-biomedica'
+const BASE_PATH = '/api/sigha-ia'
 
 export const apiFetch = (path: string, options?: RequestInit): Promise<Response> =>
   fetch(`${BASE_PATH}${path}`, options)
